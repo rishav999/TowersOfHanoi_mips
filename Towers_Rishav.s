c2d4375740d6c5415a4c2bfc92650c2384be40b3
rishav999 <rsharma@colgate.edu>
2016-09-20 11:13:50 -0400

    Update Towers_Rishav.s

--- Towers_Rishav.s
+++ Towers_Rishav.s
@@ -17,7 +17,7 @@
 # Postconditions:
 #   result (v0) steps, total number of steps required to solve puzzle
 
-towers:        addi $sp,$sp,-24	# make space on stack
+towers: addi $sp,$sp,-24	# make space on stack
 	sw $s0,0($sp)
 	sw $s1,4($sp)		# preserve registers used by this function
 	sw $ra,8($sp)		# preserve return address
@@ -37,13 +37,13 @@ if:	slti $t1,$s3,2		# numDiscs < 2
 	jal print		# call print function
 
 	addi $v0,$zero,1	# return value = 1 
-	j endif		# jump past else 
+	j endif			# jump past else 
 
 else:	addi $s1,$zero,6	# s1 = peg = 6 
-	sub $s1,$s1,$s4	# peg = peg - start 
-	sub $s1,$s1,$s5	# peg = peg - goal = 6 - start - goal 
+	sub $s1,$s1,$s4		# peg = peg - start 
+	sub $s1,$s1,$s5		# peg = peg - goal = 6 - start - goal 
 
-	addi $a0,$s3,-1	# 1st parameter = numDiscs - 1
+	addi $a0,$s3,-1		# 1st parameter = numDiscs - 1
 	add $a1, $s4, $zero	# 2nd parameter = start
 	add $a2, $s1,$zero	# 3rd parameter = peg
 	jal towers		# recursive call to towers 
@@ -53,21 +53,21 @@ else:	addi $s1,$zero,6	# s1 = peg = 6
 	add $a1,$s4,$zero	# 2nd parameter = start
 	add $a2,$s5,$zero	# 3rd parameter = goal
 	jal towers		# recursive call to towers 
-	add $s0,$v0,$s0	# steps = steps + result 
+	add $s0,$v0,$s0		# steps = steps + result 
 
-	addi $a0,$s3,-1	# 1st parameter = numDiscs - 1
+	addi $a0,$s3,-1		# 1st parameter = numDiscs - 1
 	add $a1,$zero,$s1	# 2nd parameter = peg
 	add $a2,$s5,$zero	# 3rd parameter = goal
 	jal towers		# recursive call to towers 
-	add $v0,$v0,$s0	# return value = steps + result
+	add $v0,$v0,$s0		# return value = steps + result
 
 endif:	lw $s0,0($sp)		# restore registers used by this function
 	lw $s1,4($sp)			
-	lw $ra,8($sp)	
+	lw $ra,8($sp)		# restore return address 
 	lw $s3,12($sp)
 	lw $s4,16($sp)
-	lw $s5,20($sp)		# restore return address 
-	addi $sp,$sp,24	# restore stack pointer
+	lw $s5,20($sp)		
+	addi $sp,$sp,24		# restore stack pointer
 
 	jr $ra		# return 
 
